3ae2121f9ba7641750593135cd484a7599a1cc31
Richard Klemm <richy@coding-reality.de>
2014-04-18 15:11:21 +0200

    Start remote repository creation

--- createRemoteRepros.sh
+++ createRemoteRepros.sh
@@ -1,2 +1,6 @@
 #/bin/bash
+
+# cd libavg_git/libavg
 curl -u 'klemmster' https://api.github.com/orgs/libavg/repos -d '{"name":"libavg", "homepage":"https://www.libavg.de", "has_wiki":"False", "has_downloads":"False"}'
+git remote add origin github:libavg/libavg.git
+git push -u origin master
