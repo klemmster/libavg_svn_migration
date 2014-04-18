dafafb07269e63b5f88dae79ce60f6e4ffa27c6e
Richard Klemm <richy@coding-reality.de>
2014-04-18 15:10:19 +0200

    Delete tagged branches

--- createTags.sh
+++ createTags.sh
@@ -1,5 +1,15 @@
 #/bin/bash
+
+# create tags from tag-branches
 git branch | sed s/..// | grep tag-- | sed s/tag--// |
 while read tagname; do
-        git tag -a "$tagname" -m "Tag imported from SVN." "tag--$tagname" >/dev/null 2>/dev/null && echo "tagged: $tagname"
+   git tag -a "$tagname" -m "Tag imported from SVN." "tag--$tagname" >/dev/null 2>/dev/null && echo "tagged: $tagname"
 done
+
+# delete tag -branches
+
+git branch | sed s/..// | grep tag-- |
+while read branchname; do
+    git branch -D $branchname
+done
+
